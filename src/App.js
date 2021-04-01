71e66a8731c9f7e18023ee1f3634fa766f4da129
souadmaniani <soadmaniani98@gmail.com>
2021-04-01 09:08:00 +0100

    typography

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+// } from "react-router-dom";
+import '../src/assets/sass/style.css';
 function App() {
   
   return (
-    <Router>
-        <Switch>
-        <Route path='/Register'>
-            <Register />
-        </Route>
-        <Route path='/'>
-          <SideBar></SideBar>
-        </Route>
-        </Switch>
-    </Router>
+    <div>
+      <h3>Hello world</h3>
+      <small>that is small</small>
+    </div>
+    // <Router>
+    //     <Switch>
+    //     <Route path='/Register'>
+    //         <Register />
+    //     </Route>
+    //     <Route path='/'>
+    //       <SideBar></SideBar>
+    //     </Route>
+    //     </Switch>
+    // </Router>
   );
 }
 
